7cea1a9b7c2ade72e7fa8155f23a5abb9467e5bb
Usman Turajo <ukturajo@gmail.com>
2025-11-08 13:32:17 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load model and scaler
+model = joblib.load('model.pkl')
+scaler = joblib.load('scaler.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get input values from form
+    features = [float(x) for x in request.form.values()]
+    scaled_features = scaler.transform([features])
+    prediction = model.predict(scaled_features)[0]
+    result = 'Malignant' if prediction == 1 else 'Benign'
+    return render_template('index.html', prediction_text=f'Tumor is likely: {result}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
